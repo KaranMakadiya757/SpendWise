599722a39f73991a5049fec1f02b762fb8d11737
KaranMakadiya757 <karanmakadiya87@gmail.com>
2025-08-25 11:46:00 +0530

    added multer middleware. added cloudinary for cloud services

---
+++ src/Utils/fileOperation.ts
@@ -0,0 +1,40 @@
+import fs from "fs";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath: string) => {
+    try {
+        if (!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+            folder: `spendwise/users`,
+        });
+
+        fs.unlinkSync(localFilePath);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.error("Upload Error:", error);
+        return null;
+    }
+};
+
+const deleteFromCloudinary = async (publicId: string, resourceType = "image") => {
+    try {
+        const result = await cloudinary.uploader.destroy(publicId, {
+            resource_type: resourceType,
+        });
+        return result;
+    } catch (error) {
+        console.error("Deletion Error:", error);
+        return null;
+    }
+};
+
+export { uploadOnCloudinary, deleteFromCloudinary };
